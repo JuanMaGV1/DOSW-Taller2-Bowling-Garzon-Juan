4fa80416b9c85571677fe3faaf558ee3a588e0e6
JuanMaGV1 <juan.gviracacha@mail.escuelaing.edu.co>
2026-09-14 18:24:13 -0500

    feat: cambio estructura test Bowling, agregar en gitignore sonarqube

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -1,13 +1,28 @@
 package edu.eci.dosw.bowling;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
 
+    private BowlingGame game;
+
+    @BeforeEach 
+    void setUp() {
+        game = new BowlingGame();
+    }
+
+    // ================================================
+    // MODULO A - roll() validaciones y estado
+    // ================================================
+
+    // ------------------- A1 ------------------------
+
+    // ------------------- A2 ------------------------
     @Test
-    @DisplayName("roll(-1) lanza IllegalArgumentException")
+    @DisplayName("A2: roll(-1) lanza IllegalArgumentException")
     void rollNegativePins_throwsException() {
         // Arrange
         BowlingGame game = new BowlingGame();
