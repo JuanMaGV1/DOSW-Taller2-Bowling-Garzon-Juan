54a21a4f8e6b5cf6fad9410e7d5c35d36d18ee9a
JuanMaGV1 <juan.gviracacha@mail.escuelaing.edu.co>
2026-09-14 18:06:31 -0500

    test: RED - roll lanza excepcion con pines negativos

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -1,5 +1,20 @@
 package edu.eci.dosw.bowling;
 
-public class BowlingGameTest {
-    
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+
+    @Test
+    @DisplayName("roll(-1) lanza IllegalArgumentException")
+    void rollNegativePins_throwsException() {
+        // Arrange
+        BowlingGame game = new BowlingGame();
+        // Act & Assert
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> game.roll(-1)
+        );
+    }
 }
