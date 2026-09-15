117de7af650b8297d0d8817258fc5c07b41d8af2
JuanMaGV1 <juan.gviracacha@mail.escuelaing.edu.co>
2026-09-14 20:12:48 -0500

    refactor: extrae sumRolls en BowlingScorer

--- bowling-tdd/src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
+++ bowling-tdd/src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
@@ -3,10 +3,14 @@ package edu.eci.dosw.bowling;
 import java.util.List;
 
 public class BowlingScorer {
+    
+    private int sumRolls(Frame f){
+        return f.getRolls().stream().mapToInt(Integer::intValue).sum();
+    }
     public int calculate(List<Frame> frames){
         int total = 0;
         for (Frame f : frames){
-            total += f.getRolls().stream().mapToInt(Integer::intValue).sum();
+            total += sumRolls(f);
         }
         return total;
     }
