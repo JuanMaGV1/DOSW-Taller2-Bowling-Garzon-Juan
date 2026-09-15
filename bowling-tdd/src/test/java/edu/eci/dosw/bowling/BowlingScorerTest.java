f1e8388ab5d30b353ba3a9f4af6bb175a567d5f7
JuanMaGV1 <juan.gviracacha@mail.escuelaing.edu.co>
2026-09-14 20:24:59 -0500

    tes: RED - strike suma los dos tiros siguientes

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -52,4 +52,17 @@ class BowlingScorerTest {
         rollMany(17, 0);
         assertEquals(16, game.score()); // 13 + 3 = 16
     }
+
+    // ------------------- B4 ------------------------
+    @Test
+    @DisplayName("Strike + roll(4)+roll(3) => frame 1 = 17")
+    void strike_addsNextTwoRolls() {
+        game.roll(10);
+        game.roll(4);
+        game.roll(3);
+        // frames restantes a 0
+        for (int i = 0; i < 8; i++) { game.roll(0); game.roll(0); }
+        game.roll(0); game.roll(0); // frame 10 normal
+        assertEquals(24, game.score()); // 17 + 7 = 24
+    }
 }
