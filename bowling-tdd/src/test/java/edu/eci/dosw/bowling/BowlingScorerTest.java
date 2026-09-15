b59f5ca944b1a4e0d415b9b7f9a2c29eb6aa61fd
JuanMaGV1 <juan.gviracacha@mail.escuelaing.edu.co>
2026-09-14 20:13:59 -0500

    test: RED - juego sin bonos suma directa

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -28,4 +28,16 @@ class BowlingScorerTest {
         rollMany(20, 0);
         assertEquals(0, game.score());
     }
+
+    // ------------------- B2 ------------------------
+    @Test
+    @DisplayName("Juego sin strikes ni spares suma todos los pinos")
+    void noStrikesNoSpares_sumsAllPins() {
+        // 10 frames de 3+4 = 7 => total 70
+        for (int i = 0; i < 10; i++) {
+            game.roll(3);
+            game.roll(4);
+        }
+        assertEquals(70, game.score());
+    }
 }
